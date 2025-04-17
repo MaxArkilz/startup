60a22dde25d925849a1ed853c7958b7f9e0e768e
Abrahm Bloomquist <abebloomquist@gmail.com>
2025-04-17 02:43:40 -0600

    removed authentification

--- src/app.jsx
+++ src/app.jsx
@@ -6,15 +6,15 @@ import {About} from "./about/about";
 import { MealPlan } from "./mealPlan/mealplan";
 import {Cookbook} from "./cookbook/cookbook";
 import {Shopping} from "./shopping/shopping";
-import {Login} from "./login/login";
-import { Signup } from './signup/signup';
-import { AuthState } from './login/authState'
+// import {Login} from "./login/login";
+// // import { Signup } from './signup/signup';
+// import { AuthState } from './login/authState'
 import logo from './resippyIcon.png';
 
 export default function App() {
-    const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
-    const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
-    const [authState, setAuthState] = React.useState(currentAuthState);
+    // const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+    // const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+    // const [authState, setAuthState] = React.useState(currentAuthState);
 
     return (
         <BrowserRouter>
@@ -32,7 +32,7 @@ export default function App() {
                         <span></span>
                     </label>
                     <ul className='siteNavigation'>
-                        {authState === AuthState.Authenticated && (
+                        {/* {authState === AuthState.Authenticated && (
                             <li><NavLink className='navbar' to="/mealplan">Meal Plan</NavLink></li>
                         )}
                         {authState === AuthState.Authenticated && (
@@ -44,30 +44,39 @@ export default function App() {
                         <li><NavLink className='navbar' to="/about">About</NavLink></li>
                         <li>
                             <NavLink className='loginLink' to='/'><button className='loginButton'>Login</button></NavLink>
+                        </li> */}
+                        <li><NavLink className='navbar' to="/mealplan">Meal Plan</NavLink></li>
+                        <li><NavLink className='navbar' to="/cookbook">Cookbook</NavLink></li>
+                        <li><NavLink className='navbar' to="/shopping">Shopping List</NavLink></li>
+                        <li><NavLink className='navbar' to="/about">About</NavLink></li>
+                        <li>
+                            <NavLink className='loginLink' to='/'><button className='loginButton'>Login</button></NavLink>
                         </li>
                     </ul>
                 </nav>
             </header>
             <Routes>
-                <Route path="/" element={<Login 
-                    userName={userName}
-                    authState={authState}
-                    onAuthChange={(userName, authState) => {
-                        setAuthState(authState);
-                        setUserName(userName);
-                    }}
+                {/* <Route path="/" element={<Login 
+                    // userName={userName}
+                    // authState={authState}
+                    // onAuthChange={(userName, authState) => {
+                    //     setAuthState(authState);
+                    //     setUserName(userName);
+                    // }}
                     />
-                    }t
-                    exact
-                     />
+                    }
+                     /> */}
+                <Route path="/" element={<About />} />
                 <Route path="/mealplan" element={<MealPlan />} />
                 <Route path="/cookbook" element={<Cookbook />} />
-                <Route path="/shopping" element={<Shopping userName={userName}
-                    authState={authState}
-                    onAuthChange={(userName, authState) => {
-                        setAuthState(authState);
-                        setUserName(userName);
-                    }}/>} />
+                <Route path="/shopping" element={<Shopping 
+                    // userName={userName}
+                    // authState={authState}
+                    // onAuthChange={(userName, authState) => {
+                    //     setAuthState(authState);
+                    //     setUserName(userName);
+                    // }}/>} />
+                />} />
                 <Route path="/about" element={<About/>} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path='*' element={<NotFound />} />
