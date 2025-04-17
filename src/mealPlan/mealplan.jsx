591a209248afb74b1762dc52cabf810be0827c6b
Abrahm Bloomquist <abebloomquist@gmail.com>
2025-04-16 23:20:13 -0600

    delete non-functioning apps

--- src/mealPlan/mealplan.jsx
+++ src/mealPlan/mealplan.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Calendar from './calendar';
 
 export function MealPlan() {
   return (
     <div className="MealPlan">
-      <Calendar />
+      
     </div>
   );
 }
--- src/shopping/shopping.jsx
+++ src/shopping/shopping.jsx
@@ -1,12 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './shopStyle.css';
-import { webTest, checkConnectionStatus } from './webSocket';
-import { Chat } from './chatCode';
 
 export function Shopping( {userName}) {
-  useEffect(() => {
-    webTest();
-  }, []);
 
   return (
     <main>
@@ -17,9 +12,6 @@ export function Shopping( {userName}) {
         <label><input type="checkbox" /> 2 lb. stew beef</label><br />
       </div>
     </div>
-    <button onClick={checkConnectionStatus}>Check WebSocket</button>
-    
-    <Chat />
       
   </main>
   
